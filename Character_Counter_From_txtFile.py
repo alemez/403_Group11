eaceacefe6716acf270a8e0a8306579fefe2b871
JaysonPidgeon <jayson.pidgeon@robotsandpencils.com>
2016-03-28 19:56:42 -0600

    Added a delete mechanism to the script so that data isn't appended to the end of the fine upon running of the program

--- Character_Counter_From_txtFile.py
+++ Character_Counter_From_txtFile.py
@@ -1,8 +1,11 @@
 import csv
 import string
+import os
 
 file_name = "Defective_Line_Container.txt"
 ascii_letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+spread_sheet_file = os.getcwd() + "\counted_characters.csv"
+print (spread_sheet_file)
 
 with open(file_name, 'r') as myfile:
 	characters_as_string = myfile.read()
@@ -10,7 +13,10 @@ with open(file_name, 'r') as myfile:
 from collections import Counter
 character_counter = Counter(characters_as_string)
 
-with open('counted_characters.csv', 'a', newline = '') as fp:
+if os.path.isfile(spread_sheet_file):
+	os.remove(spread_sheet_file)
+
+with open(spread_sheet_file, 'a', newline = '') as fp:
 	a = csv.writer(fp, delimiter = ',')
 	data = [["Character Counter"]]
 	a.writerows(data)
